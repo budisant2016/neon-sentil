d029b793d9c5b74f68a10ae646aa94cec836f052
budisant2016 <budisolution@gmail.com>
2025-11-07 18:08:39 +0700

    Update db_config.py

--- config/db_config.py
+++ config/db_config.py
@@ -1,1 +1,10 @@
+import streamlit as st
+from sqlalchemy import create_engine
+
+try:
+    DB_URL = st.secrets["connections"]["NEON_DB_URL"]
+except KeyError:
+    raise ValueError("❌ NEON_DB_URL belum diset di Streamlit Secrets!")
+
+engine = create_engine(DB_URL)
 
